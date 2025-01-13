d1c16df45a81e85ea67649eb6987a905aaa1d2c9
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-01-13 11:39:30 +0100

    comment

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -32,8 +32,8 @@ class WalletController extends Controller
     {
         $data = ['customer' => $customerId];
         // $data['preferred_bank'] = 'wema-bank';
-        // $data['preferred_bank'] = 'titan-paystack';
-        $data['preferred_bank'] = 'test-bank';
+        $data['preferred_bank'] = 'titan-paystack';
+        // $data['preferred_bank'] = 'test-bank';
         
         try {
             $response = Http::withToken(env('PAYSTACK_SECRET_KEY'))

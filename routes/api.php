14d107e8feb8f24ed9e82d09a30e6260fbea5ace
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-03-18 23:28:56 +0100

    comment

--- routes/api.php
+++ routes/api.php
@@ -51,8 +51,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/transfer/assets', [TransferPropertyController::class, 'index']);
     Route::get('/transfer/assets/details/{id}', [APITransferPropertyController::class, 'transferDetails']);
-    Route::post('/transfer/initiate', [TransferPropertyController::class, 'verifyRecipient']);
-    // Route::get('/transfer/recipient', [TransferPropertyController::class, 'transferRecipient']);
+    Route::post('/transfer/initiate', [TransferPropertyController::class, 'transferRecipient']);
+    Route::post('/transfer/verify', [TransferPropertyController::class, 'verifyRecipient']);
 
 
 

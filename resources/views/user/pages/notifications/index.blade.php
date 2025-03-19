ec3335afe47813ce6dc701d272fffbe13bf2d00c
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-03-19 21:25:56 +0100

    comment

--- resources/views/user/pages/notifications/index.blade.php
+++ resources/views/user/pages/notifications/index.blade.php
@@ -77,9 +77,9 @@
                                             <div class="card-body">
                                                 <div class="row ">
                                                     <div class="col-6">
-                                                        <h3 class="card-title">{{ $notification['data']['message'] }}</h3>
+                                                        <h3 class="card-title">Property Valuation Notification</h3>
                                                         <h5 class="card-title text-primary">{{ $notification['data']['property_name'] }}</h5>
-                                        
+                                                       
                                                         <div class="d-flex justify-content-between align-items-center mt-2 mb-3">
                                                             <small class="text-muted">Received {{ $notification->created_at->diffForHumans() }}</small>
                                                         </div>
@@ -87,7 +87,8 @@
                                     
                                                     <div class="col-6">
                                                         <div class="text-end">
-                                                            <a href="{{ route('user.properties.show', encrypt($notification['data']['property_id']))}}" class="btn btn-success btn-md">
+                                                            <p> <strong>Market Value:</strong> ₦{{ number_format( $notification['data']['market_value'] ) }} </p>
+                                                            <a href="{{ route('user.properties.show', encrypt($notification['data']['property_id'] ) ) }}" class="btn btn-success btn-md">
                                                                 View more
                                                             </a>
                                                         </div>

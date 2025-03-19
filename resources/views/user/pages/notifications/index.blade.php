21913fbbcc0df5ae0996bb06d949b73e4bcb85fc
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-03-19 21:18:51 +0100

    comment

--- resources/views/user/pages/notifications/index.blade.php
+++ resources/views/user/pages/notifications/index.blade.php
@@ -72,7 +72,32 @@
                                             </div>
                                            
                                         @endif
+                                        @if($notification['data']['notification_status'] == 'PropertyValuationNotification')
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="row ">
+                                                    <div class="col-6">
+                                                        <h3 class="card-title">{{ $notification['data']['message'] }}</h3>
+                                                        <h5 class="card-title text-primary">{{ $notification['data']['property_name'] }}</h5>
+                                        
+                                                        <div class="d-flex justify-content-between align-items-center mt-2 mb-3">
+                                                            <small class="text-muted">Received {{ $notification->created_at->diffForHumans() }}</small>
+                                                        </div>
+                                                    </div>
                                     
+                                                    <div class="col-6">
+                                                        <div class="text-end">
+                                                            <a href="{{ route('user.properties.show', encrypt($notification['data']['property_id']))}}" class="btn btn-success btn-md">
+                                                                View more
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                               
+                                                </div>
+                                            </div>
+                                           
+                                        </div>
+                                        @endif
 
                                     
                 

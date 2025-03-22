cbbbcd81499c27adc400dd4ad4528e42efcc9fa6
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-03-22 14:39:25 +0100

    comment

--- app/Notifications/TransferNotification.php
+++ app/Notifications/TransferNotification.php
@@ -6,6 +6,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+
 
 // class TransferNotification extends Notification
 // {

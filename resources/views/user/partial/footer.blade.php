9837deca53152634e0b87862e327fb7ea02d6b18
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-04-12 16:02:00 +0100

    comment

--- resources/views/user/partial/footer.blade.php
+++ resources/views/user/partial/footer.blade.php
@@ -8,13 +8,13 @@
 
  <!-- Scroll top bar --> 
  <button id="scroll__top"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round"  stroke-width="48" d="M112 244l144-144 144 144M256 120v292"/></svg></button>
-    
+     
  <!-- All Script JS Plugins here  -->
  <script src="{{ asset('assets/admin/js/vendor/popper.js')}}" defer="defer"></script>
  <script src="{{ asset('assets/admin/js/vendor/bootstrap.min.js')}}" defer="defer"></script>
  <script src="{{ asset('assets/admin/js/plugins/swiper-bundle.min.js')}}"></script>
  <script src="{{ asset('assets/admin/js/plugins/glightbox.min.js')}}"></script>
- 
+  
   <!-- Customscript js -->   
   <script src="{{ asset('assets/admin/js/script.js')}}"></script>
 
